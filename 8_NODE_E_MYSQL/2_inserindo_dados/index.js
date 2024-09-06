d9d23c6626fd154d067e623b0c11170891bdc2e8
FelipeSDS23 <felipe03san@gmail.com>
2024-09-06 12:11:30 -0300

    Enviando aulas 2, 3, 4 e 5 de mysql

---
+++ 8_NODE_E_MYSQL/2_inserindo_dados/index.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express()
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.static("public"))
+
+// ler o body
+app.use(
+    express.urlencoded({
+        extended: true
+    }), 
+);
+
+app.use(express.json());
+
+app.post("/books/insertbook",(req, res) => {
+
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sql = `INSERT INTO books (title, pageqty) Values ('${title}', '${pageqty}')`
+
+    conn.query(sql, (err) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect("/")
+    })
+
+})
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql2"
+})
+
+conn.connect((err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    
+    console.log("Conectado!")
+
+    app.listen(3000)
+})
