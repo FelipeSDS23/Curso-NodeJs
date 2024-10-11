755d288b90c80fddb1b7021458f3f1b66629b1dd
FelipeSDS23 <felipe03san@gmail.com>
2024-10-11 11:25:11 -0300

    Projeto Toughts 5

--- 11_TOUGHTS/routes/toughtsRoutes.js
+++ 11_TOUGHTS/routes/toughtsRoutes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const ToughtsController = require('../controllers/ToughtsController')
 
+// helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, ToughtsController.createToughts)
+router.get('/dashboard', checkAuth, ToughtsController.dashboard)
 router.get('/', ToughtsController.showToughts)
 
 module.exports = router
