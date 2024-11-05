d9300164a9e6425964d864db42190570748b3506
FelipeSDS23 <felipe03san@gmail.com>
2024-11-05 19:18:01 -0300

    Get a pet - registro de usuário finalizado, função login criada, retorno de token JWT inserido apos o registro e login

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const createUserToken = require('../helpers/create-user-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -38,6 +40,58 @@ module.exports = class UserController {
             return
         }
 
+        // create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        // create a user
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        })
         
+        try {
+            
+            const newUser = await user.save()
+            
+            await createUserToken(newUser, req, res)
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
+    }
+
+    static async login(req, res) {
+
+        const {email, password} = req.body
+
+        if (!email) {
+            res.status(422).json({"message": "O e-mail é obrigatório"})
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({"message": "A senha é obrigatória"})
+            return
+        }
+
+        // check if user exists
+        const user = await User.findOne({email: email})
+
+        if (!user) {
+            res.status(422).json({"message": "Não há usuário cadastrado com esse e-mail!"})
+            return
+        }
+
+        // check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword) {
+            res.status(422).json({"message": "Senha inválida!"})
+            return
+        }
+
+        await createUserToken(user, req, res)
     }
 }
